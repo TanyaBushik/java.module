bc34c54f33321bbd10616780f5fcab6b7a9a09e3
TanyaBushik <tanya.bushik@gmail.com>
2023-02-13 01:04:46 +0200

    level 3 finished

--- js/objects.js
+++ js/objects.js
@@ -83,81 +83,81 @@
 // console.table(friends);
 // // ищем друга по имени
 
-const findFriendByName = function (allFriends, friendName) { 
+// const findFriendByName = function (allFriends, friendName) { 
 
-    for (const friend of allFriends) {
-        console.log(friend.name);
+//     for (const friend of allFriends) {
+//         console.log(friend.name);
         
-        if (friend.name === friendName) {
-            return "Found!"
-        }
-    }
-    return "Not Found!"
-};
+//         if (friend.name === friendName) {
+//             return "Found!"
+//         }
+//     }
+//     return "Not Found!"
+// };
 
 // console.log(findFriendByName(friends, "Poly"));
 // console.log(findFriendByName(friends, "Chelsy"));
 
-const getAllNames = function (allFriends) {
+// const getAllNames = function (allFriends) {
 
-    const names = [];
+//     const names = [];
 
-    for (const friend of allFriends) {
-        names.push(friend.name);
-    }
-    return names;
-}
+//     for (const friend of allFriends) {
+//         names.push(friend.name);
+//     }
+//     return names;
+// }
 // console.log(getAllNames(friends));
 
 
 
-const getOnlineFriends = function (allFriends) {
+// const getOnlineFriends = function (allFriends) {
 
-    const onlineFriends = [];
+//     const onlineFriends = [];
 
-    for (const friend of allFriends) {
-        console.log(friend);
-        console.log(friend.online);
+//     for (const friend of allFriends) {
+//         console.log(friend);
+//         console.log(friend.online);
 
-        if (friend.online) {
-            onlineFriends.push(friend);
-        }
-    }
-    return onlineFriends;
-}
+//         if (friend.online) {
+//             onlineFriends.push(friend);
+//         }
+//     }
+//     return onlineFriends;
+// }
 
 // console.log(getOnlineFriends(friends));
 
-const getOfflineFriends = function (allFriends) {
+// const getOfflineFriends = function (allFriends) {
     
-    const offlineFriends = [];
+//     const offlineFriends = [];
 
-    for (const friend of allFriends) {
-        if (!friend.online) {
-            offlineFriends.push(friend);
-        }    
-    }
-    return offlineFriends;
-};
+//     for (const friend of allFriends) {
+//         if (!friend.online) {
+//             offlineFriends.push(friend);
+//         }    
+//     }
+//     return offlineFriends;
+// };
 // console.log(getOfflineFriends(friends));
 
-const getFriendsByStatus = function (allFriends) {
-    const friendsByStatus = {
-        online: [],
-        offline: [],
-    };
+// const getFriendsByStatus = function (allFriends) {
+//     const friendsByStatus = {
+//         online: [],
+//         offline: [],
+//     };
 
-    for (const friend of allFriends) {
-        if (friend.online) {
-           friendsByStatus.online.push(friend);
-            continue;
-        }
+//     for (const friend of allFriends) {
+//         if (friend.online) {
+//            friendsByStatus.online.push(friend);
+//             continue;
+//         }
 
-        friendsByStatus.offline.push(friend);
-    }
+//         friendsByStatus.offline.push(friend);
+//     }
 
-    return friendsByStatus;
-}
+//     return friendsByStatus;
+// }
 // console.log(getFriendsByStatus(friends));
 
 
@@ -392,7 +392,7 @@ const getFriendsByStatus = function (allFriends) {
 // const lastWeekTemps = [14, 25, 11];
 // const currentWeekTemps = [23, 17, 18];
 // const allTemps = [...lastWeekTemps, ...currentWeekTemps];
-// console.log(allTemps); 
+// console.log(allTemps);
 
 
 
@@ -420,28 +420,262 @@ const getFriendsByStatus = function (allFriends) {
 // console.log(fifth); // { propA: 5, propB: 20, propC: 15 }
 
 
-const forecast = {
-  today: {
-    low: 28,
-    high: 32,
-    icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+// // Change code below this line
+
+// const {
+//   high: highToday,
+//   low: lowToday,
+//   icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+//   tomorrow: {
+//     low: lowTomorrow,
+//     high: highTomorrow,
+//     icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+//   },
+// } = forecast;
+
+// console.log(forecast);
+
+
+
+// function calculateMeanTemperature(forecast) {
+
+//   const { today: { low: todayLow = 10, high: todayHigh = 20 },
+
+//          tomorrow: { low: tomorrowLow = 20, high: tomorrowHigh = 30 }
+// } = forecast;
+
+//   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
+
+// console.log(calculateMeanTemperature({ today: {low: 28, high: 32}, tomorrow: {low: 25, high: 29} }));
+
+
+// const scores = [89, 64, 42, 17, 93, 51, 26];
+
+// const bestScore = Math.max(...scores);
+// const worstScore = Math.min(...scores);
+
+// const authors = {
+//   kiwi: 4,
+//   poly: 7,
+//   ajax: 9,
+//   mango: 6,
+// };
+
+// const entries = Object.entries(authors);
+
+// for (const [name, rating] of entries) {
+
+  // level2
+  // const [name, rating] = entry;
+
+// level1
+  // const name = entry[0];
+  // const rating = entry[1];
+
+//   console.log(name, rating);
+// }
+
+
+// function makeTask(data) {
+//   const completed = false;
+//   const category = "General";
+//   const priority = "Normal";
+
+//   const newData = { category, priority, ...data, completed };
+
+//   return newData;
+
+// }
+
+// console.log(makeTask({}));
+// console.log(
+//   makeTask({
+//     category: "Homemade",
+//     priority: "Low",
+//     text: "Take out the trash",
+//   })
+// );
+// console.log(makeTask({ priority: "Low", text: "Choose shampoo" }));
+// console.log(makeTask({ text: "Buy bread" }));
+
+
+// Change code below this line
+// function add(...args) {
+ 
+//   let total = 0;
+
+//   for (const arg of args) {
+//     total += arg;
+//   }
+//   return total;
+// }
+
+// console.log(add(15, 27));
+// console.log(add(12, 4, 11, 48));
+// console.log(add(32, 6, 13, 19, 8));
+// console.log(add(74, 11, 62, 46, 12, 36));
+
+
+// function addOverNum(number, ...args) {
+//   let total = 0;
+
+//   for (const arg of args) {
+
+//     if (number < arg) {
+//        total += arg;
+//     }
+//   }
+
+//   return total;
+// }
+// console.log(addOverNum(50, 15, 27));
+// console.log(addOverNum(10, 12, 4, 11, 48, 10, 8));
+// console.log(addOverNum(15, 32, 6, 13, 19, 8));
+// console.log(addOverNum(20, 74, 11, 62, 46, 12, 36));
+
+// Change code below this line
+// function findMatches(array, ...args) {
+//   const matches = []; // Don't change this line
+
+//   for (const arg of args) {
+
+//     if (array.includes(arg)) {
+//       matches.push(arg);
+//     }
+//   }
+
+//   return matches;
+// }
+// console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7));
+// console.log(findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2));
+// console.log(findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41));
+// console.log(findMatches([63, 11, 8, 29], 4, 7, 16));
+
+
+// const bookShelf = {
+//   // Change code below this line
+//   books: ["The last kingdom", "The guardian of dreams"],
+//   getBooks() {
+//     return "Returning all books";
+//   },
+//   addBook(bookName) {
+//     return `Adding book ${bookName}`;
+//   },
+//   removeBook(bookName) {
+//     return `Deleting book ${bookName}`;
+//   },
+//   updateBook(oldName, newName) {
+//     return `Updating book ${oldName} to ${newName}`;
+//   },
+//   // Change code above this line
+// };
+// console.log(bookShelf.getBooks());
+// console.log(bookShelf.addBook("Haze"));
+// console.log(bookShelf.removeBook("Red sunset"));
+// console.log(bookShelf.updateBook("Sands of dune", "Dune"));
+
+
+// const bookShelf = {
+//   books: ["The last kingdom", "Haze", "The guardian of dreams"],
+//   updateBook(oldName, newName) {
+//     // Change code below this line
+
+//     const indexBook = this.books.indexOf(oldName);
+//     this.books.splice(indexBook, 1, newName);
+   
+//   },
+// };
+
+// console.log(bookShelf.updateBook("Haze", "Dungeon chronicles"));
+
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
+
+//   addPotions(potionName) {
+//     this.potions.push(potionName);
+  
+//   }
+// };
+
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//   removePotion(potionName) {
+
+//     const indexPotion = this.potions.indexOf(potionName);
+//     this.potions.splice(indexPotion, 1);
+
+//   },
+// };
+
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//   updatePotionName(oldName, newName) {
+//     // Change code below this line
+
+//     const newPotions = this.potions.indexOf(oldName);
+//     this.potions.splice(newPotions, 1, newName);
+//   },
+// };
+
+const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Dragon breath", price: 780 },
+    { name: "Stone skin", price: 520 },
+  ],
+
+  // Change code below this line
+  getPotions() {
+   
+    return this.potions;
   },
-  tomorrow: {
-    low: 27,
-    high: 31,
+
+  addPotion(newPotion) {
+
+    for (const potion of this.potions) {
+      if (newPotion.name === potion.name) {
+        return `Error! Potion ${newPotion.name} is already in your inventory!`;;
+      }
+    }
+     return this.potions.push(newPotion);
+  },
+  removePotion(potionName) {
+
+    for (let i = 0; i < this.potions.length; i += 1) {
+        const item = this.potions[i];
+        if (potionName === item.name) {
+          return this.potions.splice(i, 1);
+        }
+      }
+      return `Potion ${potionName} is not in inventory!`;
+  },
+
+  updatePotionName(oldName, newName) {
+
+    for (let i = 0; i < this.potions.length; i += 1) {
+        const item = this.potions[i];
+      if (oldName === item.name) {
+        return item.name = newName;
+      }
+    }
+      return `Potion ${oldName} is not in inventory!`;
   },
 };
-// Change code below this line
-
-const {
-  high: highToday,
-  low: lowToday,
-  icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
-  tomorrow: {
-    low: lowTomorrow,
-    high: highTomorrow,
-    icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
-  },
-} = forecast;
-
-console.log(forecast);
+// console.log(atTheOldToad.getPotions());
+// console.log(atTheOldToad.addPotion({ name: "Invisibility", price: 620 }));
+// console.log(atTheOldToad.addPotion({ name: "Power potion", price: 270 }));
+// console.log(atTheOldToad.addPotion({ name: "Dragon breath", price: 700 }));
+console.log(atTheOldToad.removePotion("Dragon breath"));
+  
